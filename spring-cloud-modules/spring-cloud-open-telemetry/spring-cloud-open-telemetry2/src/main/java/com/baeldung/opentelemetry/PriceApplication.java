2d9bef32ef0dc291caec8d24c2d40a91ca9e8762
Sejal Patel <sejal@playerzero.ai>
2024-06-05 21:01:19 -0400

    Working example of distributed trace

--- spring-cloud-modules/spring-cloud-open-telemetry/spring-cloud-open-telemetry2/src/main/java/com/baeldung/opentelemetry/PriceApplication.java
+++ spring-cloud-modules/spring-cloud-open-telemetry/spring-cloud-open-telemetry2/src/main/java/com/baeldung/opentelemetry/PriceApplication.java
@@ -1,12 +1,14 @@
 package com.baeldung.opentelemetry;
 
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PriceApplication {
-
     public static void main(String[] args) {
+        AutoConfiguredOpenTelemetrySdk.initialize();
         SpringApplication.run(PriceApplication.class, args);
     }
 }
